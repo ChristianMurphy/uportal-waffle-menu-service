8f4e48ce245f54838ed91606da139feb5d98c29e
Chris Beach <beachcoding47@gmail.com>
2018-05-10 16:04:28 -0500

    #1 Basic Spring Boot structure generated from http://start.spring.io/

---
+++ src/test/java/org/jasig/portlet/wafflemenu/WaffleMenuApplicationTests.java
@@ -0,0 +1,16 @@
+package org.jasig.portlet.wafflemenu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WaffleMenuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
